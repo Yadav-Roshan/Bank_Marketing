d08d2128c02544951d43a974eae1c15b43cd12ad
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2022-11-06 15:50:07 +0530

    Update model_methods.py

--- Deployment/model_methods.py
+++ Deployment/model_methods.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 def predict(arr):
-    with open('https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment/model.sav', 'rb') as f:
+    with open('model.sav', 'rb') as f:
         model = pickle.load(f)
     
     classes = {0: 'will not subscribe', 1:'will subscribe'}
