cbcbca83e5d112b19920e014a45722180c249b80
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2022-11-08 20:32:20 +0530

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('Deployment_test/model.pkl', 'rb'))
 
 if st.button("predict"):
     prediciton =model.predict([[ 3.0,  9.0,  1.0,  5.0,0.0,  1.0,  0.0,  1.0, 6.0,  3.0,  2.95,  2.0, 6.01,  0.0,  1.0,  1.1, 9.3994, -3.64, 4.857,  5.19,0.0]])
