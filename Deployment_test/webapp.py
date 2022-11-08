3624eaf7293343d661b4a0be8fbd79055d5c3d36
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2022-11-09 00:50:46 +0530

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -7,27 +7,7 @@ from sklearn.linear_model import LogisticRegression
 
 import base64
 
-# def set_png_as_page_bg(png_file):
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     background-repeat: no-repeat;
-#     background-attachment: scroll; # doesn't work
-#     }
-#     </style>
-#     '''
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
 def set_png_as_page_bg(png_file):
-    bin_str = get_base64(png_file) 
     page_bg_img = '''
     <style>
     .stApp {
@@ -37,9 +17,10 @@ def set_png_as_page_bg(png_file):
     background-attachment: scroll; # doesn't work
     }
     </style>
-    ''' % bin_str
+    '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
+
 
 set_png_as_page_bg('https://raw.githubusercontent.com/Yadav-Roshan/Bank_Marketing/main/Deployment_test/images/background.jpg')
 
