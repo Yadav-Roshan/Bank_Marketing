6221ce00135445e5fb23484566a9e636987594b2
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2022-11-08 20:28:18 +0530

    Add files via upload

---
+++ Deployment_test/webapp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+if st.button("predict"):
+    prediciton =model.predict([[ 3.0,  9.0,  1.0,  5.0,0.0,  1.0,  0.0,  1.0, 6.0,  3.0,  2.95,  2.0, 6.01,  0.0,  1.0,  1.1, 9.3994, -3.64, 4.857,  5.19,0.0]])
+    st.write(prediciton)
