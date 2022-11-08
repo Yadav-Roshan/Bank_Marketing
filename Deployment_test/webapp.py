35f16ab118a1befea1e0c4d3baf025e4ca04fa8f
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2022-11-08 21:37:44 +0530

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -8,7 +8,8 @@ from sklearn.linear_model import LogisticRegression
 
 X_train = pd.read_csv("https://raw.githubusercontent.com/Yadav-Roshan/Bank_Marketing/main/Deployment_test/datatsets/model_x.csv")
 y_train = pd.read_csv("https://raw.githubusercontent.com/Yadav-Roshan/Bank_Marketing/main/Deployment_test/datatsets/model_y.csv")
-
+X_train = X_train.set_index('ind')
+y_train = y_train.set_index('ind')
 model=LogisticRegression()
 model.fit(X_train, y_train)
 
