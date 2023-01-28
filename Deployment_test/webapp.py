1c096ee4561d94b9ea05e652f4bd854600916c1e
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-01-28 18:23:09 +0545

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -27,8 +27,7 @@ from streamlit_option_menu import option_menu
 
 # st.sidebar.selectbox("Navigation Bar", ("Home", "Prediction", "Contribute to Datase", "About US"))
 
-with st.sidebar:
-   selected = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+selected = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
