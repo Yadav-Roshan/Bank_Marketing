ae802007e5f1f96d95824d0cab768adff38a7f1c
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-01-30 15:08:57 +0545

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -480,10 +480,11 @@ if(selected == "About Us"):
     sec = '''
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
-        <p style = "font-family: Sans-serif;color:white; text-decoration:solid; box-shadow:5px;background-color:#da2c38; border:2px #b7b7a4; padding: 80px; font-size:20px; border-radius:2px; text-align:center">
+        <p style = "-webkit-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);-moz-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);box-shadow:20px 28px 16px -26px rgba(0, 0, 0);font-family: Sans-serif;color:white; text-decoration:solid; box-shadow:5px;background-color:#da2c38; border:2px #b7b7a4; padding: 80px; font-size:20px; border-radius:2px; text-align:center">
             <span><img src = "https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/roshan.png?raw=true" width = 300 height= 300></span><br>
             <span style = "font-size: 25px"><b>Roshan Kumar Yadav</b></span><br>
             <span>2nd Year AIML, SIT, Pune</span><br>
+            <span><b>Developer</b></span><br>
             <span style = "font-size: 40px">
                 <a href = "https://www.linkedin.com/in/roshan-yadav-942536143"><i class="bi bi-linkedin"></i></a> | 
                 <a href = "https://github.com/Yadav-Roshan"><i class="bi bi-github"></i></a> |
@@ -498,8 +499,9 @@ if(selected == "About Us"):
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
         <p style = "-webkit-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);-moz-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);box-shadow:20px 28px 16px -26px rgba(0, 0, 0);font-family: Sans-serif;color:white; text-decoration:solid; box-shadow:5px;background-color:#da2c38; border:2px #b7b7a4; padding: 80px; font-size:20px; border-radius:2px; text-align:center">
             <span><img src = "https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/roshan.png?raw=true" width = 350 height= 350></span><br>
-            <span style = "font-size: 25px"><b>Dhvani</b></span><br>
+            <span style = "font-size: 25px"><b>Dhvani Parekh</b></span><br>
             <span>2nd Year AIML, SIT, Pune</span><br>
+            <span><b>Developer</b></span><br>
             <span style = "font-size: 40px">
                 <a href = "https://www.linkedin.com/in/roshan-yadav-942536143"><i class="bi bi-linkedin"></i></a> | 
                 <a href = "https://github.com/Yadav-Roshan"><i class="bi bi-github"></i></a> |
@@ -507,3 +509,61 @@ if(selected == "About Us"):
             </span>
         </p>    
     '''
+    first.markdown(fir, unsafe_allow_html = True)
+
+    thir = '''
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
+        <p style = "-webkit-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);-moz-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);box-shadow:20px 28px 16px -26px rgba(0, 0, 0);font-family: Sans-serif;color:white; text-decoration:solid; box-shadow:5px;background-color:#da2c38; border:2px #b7b7a4; padding: 80px; font-size:20px; border-radius:2px; text-align:center">
+            <span><img src = "https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/roshan.png?raw=true" width = 350 height= 350></span><br>
+            <span style = "font-size: 25px"><b>Joshua Lobo</b></span><br>
+            <span>2nd Year AIML, SIT, Pune</span><br>
+            <span><b>Developer</b></span><br>
+            <span style = "font-size: 40px">
+                <a href = "https://www.linkedin.com/in/roshan-yadav-942536143"><i class="bi bi-linkedin"></i></a> | 
+                <a href = "https://github.com/Yadav-Roshan"><i class="bi bi-github"></i></a> |
+                <a href = "https://www.instagram.com/yadavroshan276/"><i class="bi bi-instagram"></i></a>
+            </span>
+        </p>    
+    '''
+    third.markdown(thir, unsafe_allow_html=True)
+
+footer = '''
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
+        <p style = "-webkit-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);-moz-box-shadow: 0 28px 16px -26px rgba(0, 0, 0);box-shadow:20px 28px 16px -26px rgba(0, 0, 0);font-family: Sans-serif;color:white; text-decoration:solid; box-shadow:5px;background-color:#3c1642; border:2px #b7b7a4; padding: 20px; font-size:15px; border-radius:2px; text-align:center">
+            <span style = "font-size: 15px">
+            <b>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+                <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+             </svg>
+             <a href="mailto:yadavroshan276@gmail.com"> Roshan Kumar Yadav</a>
+            </b></span><br>
+            <span style = "font-size: 15px">
+            <b>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+                <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+             </svg>
+             <a href="mailto:yadavroshan276@gmail.com"> Dhvani Parekh</a>
+            </b></span><br>
+            <span style = "font-size: 15px">
+            <b>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+                <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+             </svg>
+             <a href="mailto:yadavroshan276@gmail.com"> Joshua Lobo</a>
+            </b></span><br>
+            <span style = "font-size: 40px">
+                <a href = "https://github.com/Yadav-Roshan/Bank_Marketing/"><i class="bi bi-github"></i></a> |
+                <a href = "https://www.instagram.com/yadavroshan276/"><i class="bi bi-instagram"></i></a>
+            </span><br>
+            <span style = "font-size: 15px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-c-circle" viewBox="0 0 16 16">
+  <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z"/>
+</svg> Copyright 2022</span><br>
+        </p>    
+    '''
+
+st.markdown(footer, unsafe_allow_html = True)
