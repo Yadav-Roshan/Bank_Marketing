2b2eff25c7ef01386574eb9b561e53df618447ae
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-01-30 13:50:57 +0545

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -70,7 +70,7 @@ with st.container():
     # logo.image('https://raw.githubusercontent.com/Yadav-Roshan/Bank_Marketing/main/Deployment_test/images/bank_logo.png', width=140)
     # original_title = '<p style="font-family:Arial Black; color:#e76f51; font-size: 60px; text-align:left, text-decoration: solid">Piggy & Penny Banking Services</p>'
     # titl.markdown(original_title, unsafe_allow_html=True)
-    st.markdown('''<img src = "https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/header_new.png?raw=true" width = 1275 height = 320>''', unsafe_allow_html=True)
+    st.markdown('''<img src = "https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/header_new.png?raw=true" width = 1275 height = 320 style = "border: 2px solid black; border-radius: 5px">''', unsafe_allow_html=True)
 
 selected = option_menu(None, ["Home", "Prediction",  "Analysis", "Contribute", 'About Us'], 
     icons=['house', 'gear-wide-connected',"bar-chart-line" ,"cloud-arrow-up", 'person-rolodex'], 
