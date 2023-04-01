f5f00082b6d9b850b4908d22e721aba7eae4f7b9
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-04-01 10:31:23 +0530

    Update webapp.py

--- Deployment_test/webapp.py
+++ Deployment_test/webapp.py
@@ -91,13 +91,13 @@ selected = option_menu(None, ["Dashboard", "Dataset", "Prediction",  "Analysis",
 if(selected=="Dashboard"):
     with st.expander("Decision Making Facts", expanded = True):
         education, marital, senior = st.columns(3)
-        education.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/education_card.png?raw=true")
-        marital.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/marital_card.png?raw=true")
-        senior.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/senior_card.png?raw=true")
+        education.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/education_card.jpg?raw=true")
+        marital.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/marital_card.jpg?raw=true")
+        senior.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/senior_card.jpg?raw=true")
         age, contact, loan = st.columns(3)
-        age.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/age_card.png?raw=true")
-        contact.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/contact_card.png?raw=true")
-        loan.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/loan_card.png?raw=true")
+        age.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/age_card.jpg?raw=true")
+        contact.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/contact_card.jpg?raw=true")
+        loan.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/loan_card.jpg?raw=true")
     with st.expander("Metrics", expanded = True):
         st.image(r"https://github.com/Yadav-Roshan/Bank_Marketing/blob/main/Deployment_test/images/Metrics.png?raw=true")
     
